34eb6b5bc967fbf2efdce7ca61f2c8e64d0b32bb
mudkipmaster <acg59@drexel.edu>
2017-12-06 16:36:39 -0500

    fixed bug where tweet cleaner was not sending anything back to kafka

--- pythontweetcleaner/main.py
+++ pythontweetcleaner/main.py
@@ -6,44 +6,53 @@ os.environ["PYSPARK_SUBMIT_ARGS"] = '--packages org.apache.spark:spark-streaming
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+from kafka import SimpleProducer
+from kafka import SimpleClient
+from kafka.errors import KafkaUnavailableError
+from kafka.errors import FailedPayloadsError
 from ConfigReader import ConfigReader
 from py4j.protocol import Py4JJavaError
 
 config = ConfigReader("config.json")
-
 zookeeper_url = "{:s}:{:s}".format(config.get_key("ZOOKEEPER_HOST"), config.get_key("ZOOKEEPER_PORT"))
 kafka_url = "{:s}:{:s}".format(config.get_key("KAFKA_HOST"), config.get_key("KAFKA_PORT"))
 kafka_topic = config.get_key("KAFKA_TOPIC")
 output_topic = config.get_key("KAFKA_OUTPUT_TOPIC")
 
-sc = SparkContext(appName="PythonSparkStreamingKafka")
+# input stream
+sc = SparkContext(appName="PythonTweetCleaner")
 sc.setLogLevel("WARN")
 ssc = StreamingContext(sc, 10)
 
-kafka_params = {"startingOffset": "earliest"}
-
+kafka_params = {"startingOffsets": "earliest"}
 kafkaStream = KafkaUtils.createStream(ssc, zookeeper_url, 'spark-streaming',
                                       {kafka_topic: 1}, kafka_params)
+# output stream
+try:
+    kafka = SimpleClient("192.168.99.100:9092")
+except KafkaUnavailableError as e:
+    logging.error("Could not connect to Kafka")
+    raise e
+
+producer = SimpleProducer(kafka)
 
 
 def handler(message):
-    records = message.collect()
-    for record in records:
-        producer.send('spark.out', str(record))
-        producer.flush()
+    cleaned_tweets = message.collect()
+    try:
+        for tweet in cleaned_tweets:
+            producer.send_messages(output_topic, json.dumps(tweet).encode('utf-8'))
+    except FailedPayloadsError:
+        logging.error("Could not connect to Kafka")
 
 
+# actual cleaning
+# filter all that aren't a reply
+# filter all the truncated tweets
 lines = kafkaStream.map(lambda x: json.loads(x[1])).filter(lambda tweet: tweet["in_reply_to_status_id"] != None).filter(
     lambda tweet: tweet["truncated"] == False)
 
-try:  # if there is nowhere to send our cleaned tweets, exit
-    kvs = KafkaUtils.createDirectStream(ssc, [output_topic], {
-        "metadata.broker.list": kafka_url})
-    kvs.foreachRDD(handler)
-except Py4JJavaError as e:
-    logging.error("Could not connect to Kafka")
-    exit(1)
-
+lines.foreachRDD(handler)  # send cleaned tweets to kafka
 lines.pprint()
 
 ssc.start()
